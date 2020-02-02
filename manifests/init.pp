a4aca08c491a6f3c0640d60bb84ca62b65f7c91a
Jackson Hughes <jxhughes95@gmail.com>
2020-02-02 18:08:45 +0000

    add class

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,11 +1,13 @@
-file { '/usr/local/bin/run-puppet':
-  source => '/etc/puppetlabs/code/environments/production/scripts/run-puppet.sh',
-  ensure => link,
-  target => '/usr/local/bin/run-puppet',
-}
+class puppet-cron {
+  file { '/usr/local/bin/run-puppet':
+    source => '/etc/puppetlabs/code/environments/production/scripts/run-puppet.sh',
+    ensure => link,
+    target => '/usr/local/bin/run-puppet',
+  }
 
-cron { 'run-puppet':
-  command => '/usr/local/bin/run-puppet',
-  hour    => '*',
-  minute  => '*/15',
+  cron { 'run-puppet':
+    command => '/usr/local/bin/run-puppet',
+    hour    => '*',
+    minute  => '*/15',
+  }
 }
