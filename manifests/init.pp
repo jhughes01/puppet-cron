03977091e217e81a7621a0052c5e502540bf9068
Jackson Hughes <jxhughes95@gmail.com>
2020-02-02 17:53:45 +0000

    Add puppet cron

---
+++ manifests/init.pp
@@ -0,0 +1,11 @@
+file { '/usr/local/bin/run-puppet':
+  source => '/etc/puppetlabs/code/environments/production/scripts/run-puppet.sh',
+  ensure => link,
+  target => '/usr/local/bin/run-puppet',
+}
+
+cron { 'run-puppet':
+  command => '/usr/local/bin/run-puppet',
+  hour    => '*',
+  minute  => '*/15',
+}
