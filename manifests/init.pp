3daacd61ccd3082bb5d4452f6721436389050276
Jackson Hughes <jxhughes95@gmail.com>
2020-02-10 21:20:49 +0000

    add bash to cron command

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,7 +6,7 @@ class puppetcron {
   }
 
   cron { 'run-puppet':
-    command => '/usr/local/bin/run-puppet &>> /var/log/puppet-cron.log',
+    command => 'bash /usr/local/bin/run-puppet &>> /var/log/puppet-cron.log',
     hour    => '*',
     minute  => '*/15',
   }
